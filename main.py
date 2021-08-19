8eeafb33525ca8357feac7f45c6c69171a480ede
Matheus Martins <matheusfelipeferreiramartins@gmail.com>
2021-08-18 22:08:55 -0300

    Adds unique query with every data

--- main.py
+++ main.py
@@ -17,152 +17,41 @@ def print_query_result(query_result):
         print(rf)
 
 
-defaultQuery = """
+repositories_query = """
 {
   search(query:"stars>:100", type:REPOSITORY, first:100){
      nodes {
          ... on Repository {
              nameWithOwner
+             url
+             stargazers {
+                totalCount
+             }
+             createdAt
+             pullRequests(states:MERGED) {
+                totalCount
+             }
+             releases {
+                totalCount
+             }
+             updatedAt
+             primaryLanguage{
+                name
+             }
+             open: issues(states:OPEN) {
+                totalCount
+             }
+             closed: issues(states:CLOSED) {
+                totalCount
+             }
          }
      }
   }
 }
 """
 
-query_old_repositories = """
-{
-  search(query: "stars:>100", type: REPOSITORY, first: 100) {
-    nodes {
-      ... on Repository {
-        nameWithOwner
-        createdAt
-      }
-    }
-  }
-}
-"""
 
-query_contributions_repositories = """
-{
-  search(query: "stars:>100", type: REPOSITORY, first: 100) {
-    nodes {
-      ... on Repository {
-        nameWithOwner
-        pullRequests{
-          totalCount
-        }
-      }
-    }
-  }
-}
-"""
-
-query_releases_repositories = """
-{
-  search(query: "stars:>100", type: REPOSITORY, first: 100) {
-    nodes {
-      ... on Repository {
-        nameWithOwner
-        releases{
-          totalCount
-        }
-      }
-    }
-  }
-}
-"""
-
-
-query_last_update_repositories = """
-{
-  search(query: "stars>:100", type: REPOSITORY, first: 100) {
-  nodes {
-      ... on Repository {
-        nameWithOwner
-        updatedAt
-      }
-    }
-  }
-}
-"""
-
-query_primary_language_repositories = """
-{
-  search(query: "stars>:100", type: REPOSITORY, first: 100) {
-    nodes {
-      ... on Repository {
-        nameWithOwner
-        primaryLanguage{
-          name
-        }
-      }
-    }
-  }
-}
-"""
-
-query_closed_issues_from_repositories = """
-{ 
-  search(query: "stars:>100", type: REPOSITORY, first: 100) {
-    nodes {
-      ... on Repository {
-        nameWithOwner
-        issues(states:CLOSED){
-          totalCount
-        }
-      }
-    }
-  }
-}
-"""
-
-query_issues_from_repositories = """
-{ 
-  search(query: "stars:>100", type: REPOSITORY, first: 100) {
-    nodes {
-      ... on Repository {
-        nameWithOwner
-        issues {
-          totalCount
-        }
-      }
-    }
-  }
-}
-"""
-
-
-# Get created date of popular repositories.
-print("Repositories with its created date.")
-print_query_result(run_query(query_old_repositories))
-print("\n///////////////////////////////////////////////////////////////////////////////////////////\n")
-
-# Get total pull requests of popular repositories.
-print("Repositories with number of pull requests")
-print_query_result(run_query(query_contributions_repositories))
-print("\n///////////////////////////////////////////////////////////////////////////////////////////\n")
-
-# Get total releases of popular repositories.
-print("Repositories with number of releases")
-print_query_result(run_query(query_releases_repositories))
-print("\n///////////////////////////////////////////////////////////////////////////////////////////\n")
-
-# Get last update of popular repositories.
-print("Repositories with last update date")
-print_query_result(run_query(query_last_update_repositories))
-print("\n///////////////////////////////////////////////////////////////////////////////////////////\n")
-
-# Get primary language of popular repositories.
-print("Repositories with primary language")
-print_query_result(run_query(query_primary_language_repositories))
-print("\n///////////////////////////////////////////////////////////////////////////////////////////\n")
-
-# Get closed issues count of popular repositories.
-print("Repositories with count of closed issues")
-print_query_result(run_query(query_closed_issues_from_repositories))
-print("\n///////////////////////////////////////////////////////////////////////////////////////////\n")
-
-# Get total issues count of popular repositories.
-print("Repositories with count of issues")
-print_query_result(run_query(query_issues_from_repositories))
+# Get data of popular repositories.
+print("Popular repositories data")
+print_query_result(run_query(repositories_query))
 print("\n///////////////////////////////////////////////////////////////////////////////////////////\n")
