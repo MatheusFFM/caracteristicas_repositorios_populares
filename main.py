c393eecec52bc9ce538f09338e5d6e05b9b4a1e4
Matheus Martins <matheusfelipeferreiramartins@gmail.com>
2021-08-22 14:44:08 -0300

    Adds pagination

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import requests
 
-headers = {"Authorization": "bearer <your git token>"}
+headers = {"Authorization": "bearer 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"}
 
 
-def run_query(query):
+def run_query(after):
+    after_formatted = "null" if after is None else "\"" + after + "\""
+    query = """
+    {
+      search(query:"stars:>100", type:REPOSITORY, first:100, after:""" + after_formatted + """){
+        pageInfo {
+            endCursor
+            hasNextPage
+        } 
+         nodes {
+             ... on Repository {
+                 nameWithOwner
+                 url
+                 stargazers {
+                    totalCount
+                 }
+                 createdAt
+                 pullRequests(states:MERGED) {
+                    totalCount
+                 }
+                 releases {
+                    totalCount
+                 }
+                 updatedAt
+                 primaryLanguage{
+                    name
+                 }
+                 open: issues(states:OPEN) {
+                    totalCount
+                 }
+                 closed: issues(states:CLOSED) {
+                    totalCount
+                 }
+             }
+         }
+      }
+    }
+    """
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
@@ -17,41 +54,13 @@ def print_query_result(query_result):
         print(rf)
 
 
-repositories_query = """
-{
-  search(query:"stars>:100", type:REPOSITORY, first:100){
-     nodes {
-         ... on Repository {
-             nameWithOwner
-             url
-             stargazers {
-                totalCount
-             }
-             createdAt
-             pullRequests(states:MERGED) {
-                totalCount
-             }
-             releases {
-                totalCount
-             }
-             updatedAt
-             primaryLanguage{
-                name
-             }
-             open: issues(states:OPEN) {
-                totalCount
-             }
-             closed: issues(states:CLOSED) {
-                totalCount
-             }
-         }
-     }
-  }
-}
-"""
-
-
-# Get data of popular repositories.
-print("Popular repositories data")
-print_query_result(run_query(repositories_query))
-print("\n///////////////////////////////////////////////////////////////////////////////////////////\n")
+pages = 10
+afterCode = None
+for page in range(pages):
+    print(f"\n========== Page {page+1} ==========\n")
+    result = run_query(afterCode)
+    print_query_result(result)
+    has_next = result["data"]["search"]["pageInfo"]["hasNextPage"]
+    if not has_next:
+        break
+    afterCode = result["data"]["search"]["pageInfo"]["endCursor"]
