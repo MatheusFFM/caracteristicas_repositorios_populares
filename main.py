6ff9c4c89c2c8e2088f94dd01c5ff17109c43c2d
Matheus Martins <matheusfelipeferreiramartins@gmail.com>
2021-08-22 16:20:30 -0300

    Save repositories on file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
+import csv
 from datetime import date
 
-headers = {"Authorization": "bearer <Your Token API>"}
+headers = {"Authorization": "bearer <Your API Token>"}
 
 
 def run_query(after):
@@ -55,7 +56,7 @@ def print_query_result(query_result):
         print(rf)
 
 
-def save_on_file(query_result):
+def save_on_file(query_result, writer):
     result_format = query_result["data"]["search"]["nodes"]
     for rf in result_format:
         today = date.today()
@@ -68,21 +69,30 @@ def save_on_file(query_result):
         delta_updated = today - last_update
         name = rf["nameWithOwner"]
         age_in_days = delta_created.days
-        total_pr_accepteds = rf["pullRequests"]["totalCount"]
+        total_pr_accepts = rf["pullRequests"]["totalCount"]
         total_releases = rf["releases"]["totalCount"]
         last_updated_interval = delta_updated.days
         primary_language = "" if rf["primaryLanguage"] is None else rf["primaryLanguage"]["name"]
         closed_issues_ratio = rf["closed"]["totalCount"] / total_issues
+        data = [name, age_in_days, total_pr_accepts, total_releases,
+                last_updated_interval, primary_language, closed_issues_ratio]
+        writer.writerow(data)
 
 
 pages = 10
 afterCode = None
+header = ['Name', 'Age', 'Total PR Accepts', 'Total Releases',
+          'Last Updated Interval', 'Language', 'Closed Issues Ratio']
+f = open('repositories.csv', 'w')
+w = csv.writer(f)
+w.writerow(header)
 for page in range(pages):
     print(f"\n========== Page {page + 1} ==========\n")
     result = run_query(afterCode)
     print_query_result(result)
-    save_on_file(result)
+    save_on_file(result, w)
     has_next = result["data"]["search"]["pageInfo"]["hasNextPage"]
     if not has_next:
         break
     afterCode = result["data"]["search"]["pageInfo"]["endCursor"]
+f.close()
