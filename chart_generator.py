fb40359971d566700244f409b338b7715eb4ca6b
Matheus Martins <matheusfelipeferreiramartins@gmail.com>
2021-08-29 17:52:46 -0300

    Adds analysis by language

--- chart_generator.py
+++ chart_generator.py
@@ -11,8 +11,31 @@ class Rows(Enum):
     CLOSED_ISSUES_RATIO = "Closed Issues Ratio"
 
 
+class PopularLanguages(Enum):
+    JS = "JavaScript"
+    PYTHON = "Python"
+    JAVA = "Java"
+    TYPESCRIPT = "TypeScript"
+    CSHARP = "C#"
+    PHP = "PHP"
+    CPLUSPLUS = "C++"
+    C = "C"
+    SHELL = "Shell"
+    RUBY = "Ruby"
+
+
 def generate_chart(origin, column, filename):
     origin.boxplot(column=column)
+    plt.savefig(f"{filename}.png")
+    plt.close()
+
+
+def generate_chart_by_language(origin, column, filename, group):
+    origin.boxplot(column=column,
+                   by=[group],
+                   rot=90,
+                   grid=False,
+                   fontsize=5)
     plt.savefig(f"{filename}.png")
     plt.close()
 
@@ -21,3 +44,7 @@ data = pd.read_csv("repositories.csv")
 for row in Rows:
     print(f"Creating {row.value} boxplot")
     generate_chart(data, row.value, row.value.replace(" ", ""))
+
+generate_chart_by_language(data, Rows.CLOSED_ISSUES_RATIO.value, "popularLanguagesIssues", "Language")
+generate_chart_by_language(data, Rows.TOTAL_RELEASES.value, "popularLanguagesReleases", "Language")
+generate_chart_by_language(data, Rows.TOTAL_PR_ACCEPTS.value, "popularLanguagesPR", "Language")
