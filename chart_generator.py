e06a27654f3688f57e50cfe547ec79e9784a7db0
Matheus Martins <matheusfelipeferreiramartins@gmail.com>
2021-08-29 18:22:22 -0300

    Removes languages enum

--- chart_generator.py
+++ chart_generator.py
@@ -11,19 +11,6 @@ class Rows(Enum):
     CLOSED_ISSUES_RATIO = "Closed Issues Ratio"
 
 
-class PopularLanguages(Enum):
-    JS = "JavaScript"
-    PYTHON = "Python"
-    JAVA = "Java"
-    TYPESCRIPT = "TypeScript"
-    CSHARP = "C#"
-    PHP = "PHP"
-    CPLUSPLUS = "C++"
-    C = "C"
-    SHELL = "Shell"
-    RUBY = "Ruby"
-
-
 def generate_chart(origin, column, filename):
     origin.boxplot(column=column)
     plt.savefig(f"{filename}.png")
@@ -45,6 +32,7 @@ for row in Rows:
     print(f"Creating {row.value} boxplot")
     generate_chart(data, row.value, row.value.replace(" ", ""))
 
+print("Creating boxplot for each language")
 generate_chart_by_language(data, Rows.CLOSED_ISSUES_RATIO.value, "popularLanguagesIssues", "Language")
 generate_chart_by_language(data, Rows.TOTAL_RELEASES.value, "popularLanguagesReleases", "Language")
 generate_chart_by_language(data, Rows.TOTAL_PR_ACCEPTS.value, "popularLanguagesPR", "Language")
